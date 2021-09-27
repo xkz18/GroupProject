503431c64de31673d08b453c12e2e025bbf09d4e
Andy Xu <59854290+xu51a12@users.noreply.github.com>
2021-09-27 15:47:41 -0500

    Spring boot setup + entities tables

---
+++ demo/src/main/java/ResourceManager/model/User.java
@@ -0,0 +1,14 @@
+package ResourceManager.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+}

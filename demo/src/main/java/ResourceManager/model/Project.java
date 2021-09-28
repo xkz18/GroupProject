228927b39e2b5e4b02a5f0d374df4a8529a460f1
Andy Xu <59854290+xu51a12@users.noreply.github.com>
2021-09-27 20:05:36 -0500

    Added models for user and project

---
+++ demo/src/main/java/ResourceManager/model/Project.java
@@ -0,0 +1,49 @@
+package ResourceManager.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@Entity
+@Table(name = "project")
+public class Project {
+    @Id
+    @GeneratedValue
+    private int projectId;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name="user_id")
+    private User user;
+
+    public Project() {
+
+    }
+    public Project(Date dateCreated) {
+        this.dateCreated = dateCreated;
+
+    }
+    public int getId() {
+        return projectId;
+    }
+    public void setId(int id) {
+        this.projectId = id;
+    }
+    public Date getDateCreated(){
+        return dateCreated;
+    }
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+    @Override
+    public String toString() {
+        return "Project [id=" + projectId +", DateCreated=" + dateCreated + "]";
+    }
+
+
+}

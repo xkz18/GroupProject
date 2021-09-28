5125072ff895bf29fe791f4e9f8529a850fbc22f
Andy Xu <59854290+xu51a12@users.noreply.github.com>
2021-09-28 13:14:40 -0500

    entities for project and user

---
+++ demo/src/main/java/ResourceManager/model/User.java
@@ -0,0 +1,87 @@
+package ResourceManager.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.sql.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer id;
+
+    @NotEmpty
+    @Column(nullable = false, unique = true, name = "username")
+    private String username;
+
+    @NotEmpty
+    @Column(name="password")
+    private String password;
+
+    @Column(name = "date_created")
+    private Date dateCreated;
+
+    @Column(name = "last_updated")
+    private Date lastUpdated;
+
+    @OneToMany(fetch=FetchType.LAZY,
+            mappedBy="users",
+            cascade= {CascadeType.PERSIST, CascadeType.MERGE,
+                    CascadeType.DETACH, CascadeType.REFRESH})
+    private List<Project> projects;
+
+    public User() {
+
+    }
+    public User(String username, String password, Date dateCreated) {
+        this.username = username;
+        this.password = password;
+        this.dateCreated = dateCreated;
+
+    }
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public Date getDateCreated(){
+        return dateCreated;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public void setPassword(String encodedPassword) {
+        this.password = encodedPassword;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", userName=" + username + ", DateCreated=" + dateCreated + ", LastUpdated=" + lastUpdated
+                + "]";
+    }
+
+}

10a807c388b4737f11a0eaeb5749ee5a697964a1
Andy Xu <59854290+xu51a12@users.noreply.github.com>
2021-10-02 17:42:13 -0500

    added controllers for signup and login (Work in progress)

--- demo/src/main/java/ResourceManager/repository/UserRepository.java
+++ demo/src/main/java/ResourceManager/repository/UserRepository.java
@@ -5,4 +5,5 @@ import ResourceManager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User getUserByUsername(String username);
 }

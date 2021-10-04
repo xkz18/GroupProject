d240b8465f3c13e98a0a4a59e54ed5a088729006
Huijun-Sun <huijun123xiamen@outlook.com>
2021-10-04 14:22:45 -0400

    Update ProjectColumnController.java

--- demo/src/main/java/com/example/demo/Controller/ProjectColumnController.java
+++ demo/src/main/java/com/example/demo/Controller/ProjectColumnController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
+@RequestMapping("/projectColumn")
 public class ProjectColumnController {
     @Autowired
     ProjectColumnService service;

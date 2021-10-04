c1a6e08a01b7b90eb3449f42e75c4fe405707a66
Huijun-Sun <huijun123xiamen@outlook.com>
2021-10-04 13:47:22 -0400

    ProjectColumn Controller

--- demo/src/main/java/com/example/demo/Controller/ProjectColumnController.java
+++ demo/src/main/java/com/example/demo/Controller/ProjectColumnController.java
@@ -1,45 +1,44 @@
 package com.example.demo.Controller;
 
+import com.example.demo.ResourceManager.model.ProjectColumns;
 import com.example.demo.ResourceManager.model.ProjectResources;
+import com.example.demo.Service.ProjectColumnService;
 import com.example.demo.Service.ProjectResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
-import java.beans.Transient;
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping("/projectResource")
-public class ProjectResourceController {
+@RequestMapping("/projectColumn")
+public class ProjectColumnController {
     @Autowired
-    ProjectResourceService service;
+    ProjectColumnService service;
 
     @PostMapping("/create")
-    public ProjectResources addProjectResource(@RequestBody ProjectResources record){
-        record.setTime_created(new Date(System.currentTimeMillis()));
-        return service.addProjectResource(record);
+    public ProjectColumns addProjectResource(@RequestBody ProjectColumns column){
+        return service.addColumn(column);
     }
 
     @PostMapping("/delete")
     public void deleteProjectResource(@RequestParam Integer id){
-        service.deleteProjectResource(id);
+        service.deleteColumn(id);
     }
 
     @GetMapping("/getAll")
     @Transactional
-    public ResponseEntity<List<ProjectResources>> getAllResource(){
-        List<ProjectResources> list = service.getAllProjectResources();
+    public ResponseEntity<List<ProjectColumns>> getAllResource(){
+        List<ProjectColumns> list = service.getAllColumns();
         System.out.println(list.get(0));
         return ResponseEntity.ok(list);
         //return new ResponseEntity<>(list, HttpStatus.OK);
     }
     @GetMapping
     public String getTest(){
-        return "Test";
+        return "column_test";
     }
 }
